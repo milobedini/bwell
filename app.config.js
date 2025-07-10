a9bc8481fca555ecbb8ad0915bf106562bb697f1
Milo Bedini <milobedini64@gmail.com>
2025-07-10 08:51:17 +0100

    app config

--- app.config.js
+++ app.config.js
@@ -2,10 +2,57 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: 'MyApp',
-    slug: 'myapp',
+    name: 'bwell',
+    slug: 'bwell',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'bwell',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    owner: 'milobedini',
+    runtimeVersion: {
+      policy: 'appVersion'
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff'
+      },
+      edgeToEdgeEnabled: true
+    },
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png'
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-splash-screen',
+        {
+          image: './assets/images/splash-icon.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff'
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true
+    },
     extra: {
-      BACKEND_BASE_URL: process.env.BACKEND_BASE_URL
+      router: {},
+      BACKEND_BASE_URL: process.env.BACKEND_BASE_URL,
+      eas: {
+        projectId: '2bff059e-0e09-4d11-8414-e062119c44a5' // ✅ copy this from your current app.json
+      }
+    },
+    updates: {
+      url: 'https://u.expo.dev/2bff059e-0e09-4d11-8414-e062119c44a5'
     }
   }
 };
