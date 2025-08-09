7d734c384dc617d19dd0fbd2e47c4b32e786f3f0
Milo Bedini <milobedini64@gmail.com>
2025-08-09 11:18:09 +0100

    headers

--- app/(main)/(tabs)/programs/_layout.tsx
+++ app/(main)/(tabs)/programs/_layout.tsx
@@ -1,12 +1,12 @@
 import { Stack } from 'expo-router';
-import { nestedScreenOptions, stackScreenOptions } from '@/utils/defaultScreenOptions';
+import { nestedScreenOptionsWithTitle, stackScreenOptions } from '@/utils/defaultScreenOptions';
 
 export default function ProgramsStack() {
   return (
     <Stack screenOptions={stackScreenOptions}>
       <Stack.Screen name="index" />
-      <Stack.Screen name="[id]/index" options={nestedScreenOptions} />
-      <Stack.Screen name="[id]/modules" options={nestedScreenOptions} />
+      <Stack.Screen name="[id]/index" options={nestedScreenOptionsWithTitle('Program modules')} />
+      <Stack.Screen name="[id]/modules" options={nestedScreenOptionsWithTitle('Module')} />
     </Stack>
   );
 }
