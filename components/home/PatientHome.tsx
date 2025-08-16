0f8206b9a061defe0f1b2d9d9566d4118e5682d8
Milo Bedini <milobedini64@gmail.com>
2025-08-16 11:04:50 +0100

    home refactors

--- components/home/PatientHome.tsx
+++ components/home/PatientHome.tsx
@@ -1,21 +1,16 @@
-import { View } from 'react-native';
-
-import { BWellLogo } from '../brand/Imagery';
-import Container from '../Container';
 import ContentContainer from '../ContentContainer';
 import { ThemedText } from '../ThemedText';
 
+import { HomeScreen } from './HomeScreen';
+
 const PatientHome = () => {
-  return (
-    <Container>
-      <ContentContainer>
-        <ThemedText>Patient home</ThemedText>
-        <View className="mt-auto items-center">
-          <BWellLogo />
-        </View>
-      </ContentContainer>
-    </Container>
+  const content = (
+    <ContentContainer>
+      <ThemedText>Patient home</ThemedText>
+    </ContentContainer>
   );
+
+  return <HomeScreen content={content} />;
 };
 
 export default PatientHome;
--- components/home/VerifiedTherapistHome.tsx
+++ components/home/VerifiedTherapistHome.tsx
@@ -1,29 +1,21 @@
-import { View } from 'react-native';
 import { useRouter } from 'expo-router';
 
-import { BWellLogo } from '../brand/Imagery';
-import Container from '../Container';
 import ContentContainer from '../ContentContainer';
-import ThemedButton from '../ThemedButton';
-import { ThemedText } from '../ThemedText';
+import { PrimaryButton } from '../ThemedButton';
+
+import { HomeScreen } from './HomeScreen';
 
 const VerifiedTherapistHome = () => {
   const router = useRouter();
 
-  return (
-    <Container>
-      <ContentContainer>
-        <ThemedText>Verified therapist home</ThemedText>
-        <ThemedButton className="mb-4" onPress={() => router.push('/home/clients')}>
-          Your clients
-        </ThemedButton>
-        <ThemedButton onPress={() => router.push('/home/patients')}>All patients</ThemedButton>
-        <View className="mt-auto items-center">
-          <BWellLogo />
-        </View>
-      </ContentContainer>
-    </Container>
+  const content = (
+    <ContentContainer>
+      <PrimaryButton title="Your clients" onPress={() => router.push('/home/clients')} />
+      <PrimaryButton title="All patients" onPress={() => router.push('/home/patients')} />
+    </ContentContainer>
   );
+
+  return <HomeScreen content={content} />;
 };
 
 export default VerifiedTherapistHome;
