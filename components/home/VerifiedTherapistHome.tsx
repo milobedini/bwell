f1a2f13474c432c993a7ea0cc0599ccf02184d80
Milo Bedini <milobedini64@gmail.com>
2026-03-24 19:16:20 +0000

    fix: remove animated backdrop from therapist home to allow full dashboard scroll

--- components/home/VerifiedTherapistHome.tsx
+++ components/home/VerifiedTherapistHome.tsx
@@ -1,15 +1,24 @@
+import { View } from 'react-native';
+import Constants from 'expo-constants';
+import { Image } from 'expo-image';
 import { useProfile } from '@/hooks/useUsers';
 
-import { HomeScreen } from './HomeScreen';
 import TherapistDashboard from './TherapistDashboard';
+
+import bWellLogo from '../../assets/images/logo.png';
 
 const VerifiedTherapistHome = () => {
   const { data: profile } = useProfile();
   const firstName = profile?.name?.split(' ')[0] || profile?.username || '';
 
-  const content = <TherapistDashboard firstName={firstName} />;
-
-  return <HomeScreen content={content} />;
+  return (
+    <View className="flex-1 bg-sway-dark">
+      <View className="flex-row justify-center pb-[22]" style={{ marginTop: Constants.statusBarHeight + 22 }}>
+        <Image source={bWellLogo} style={{ aspectRatio: 2000 / 1247, width: 140 }} />
+      </View>
+      <TherapistDashboard firstName={firstName} />
+    </View>
+  );
 };
 
 export default VerifiedTherapistHome;
