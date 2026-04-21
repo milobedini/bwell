7b19bd620c472055b4473c46a6d9650e0c4dccaa
Milo Bedini <milobedini64@gmail.com>
2026-04-21 18:07:03 +0200

    feat(admin): register programmes/[id] screen in home stack

--- app/(main)/(tabs)/home/_layout.tsx
+++ app/(main)/(tabs)/home/_layout.tsx
@@ -1,11 +1,16 @@
 import { Stack } from 'expo-router';
 import { stackScreenOptions, withHeaderFromParams } from '@/utils/defaultScreenOptions';
 
+export const unstable_settings = {
+  initialRouteName: 'index'
+};
+
 export default function HomeStack() {
   return (
     <Stack screenOptions={stackScreenOptions}>
       <Stack.Screen name="index" />
       <Stack.Screen name="practice/[id]" options={withHeaderFromParams()} />
+      <Stack.Screen name="programmes/[id]" options={withHeaderFromParams()} />
     </Stack>
   );
 }
